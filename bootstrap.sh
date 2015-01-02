ffb33c9a7dc8399c79d4d2a8f723a68d012c4b1f
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-01-02 11:37:46 +0000

    restructure folder

---
+++ bootstrap.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+cur=$(dirname "${BASH_SOURCE}");
+cd $cur;
+
+function doIt() {
+    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
+    --exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
+    
+    source ~/.bashrc;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+    doIt;
+    else
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    echo "";
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+	doIt;
+	fi;
+    fi;
+unset doIt;
