d8f14dca5ff0291cdc9ef41d2953e6d66eff3c62
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2015-02-27 01:58:11 -0500

    param home directory

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,8 @@ cur=$(pwd);
 cd $cur;
 
 function doIt() {
+    home=$1;
+    echo "Setting files in $home";
     for dotfile in .?*; do
 	case $dotfile in
 	    *.elc)
@@ -13,23 +15,27 @@ function doIt() {
 	    .git)
 		continue;;
 	    *)
-		ln $flag  "$cur/${dotfile}" /home/;
+		ln $flag  "$cur/${dotfile}" $home;
 		;;
 	esac
     done
 
-    ln $flag "$cur/tmux-config/.tmux.conf" /home/;
+    ln $flag "$cur/tmux-config/.tmux.conf" $home;
     source ~/.bashrc;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-    doIt;
+    doIt $HOME;
 else
-    read -p "This may overwrite existing files in your home directory ($HOME). Are you sure? (y/n) " -n 1;
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
     echo "";
     if  [[ $REPLY=='Y' || $REPLY=='y' ]]; then
-    # if [[ $REPLY =~ ^[Yy]$ ]]; then
-	doIt;
+	read -p "Home directory (default: $HOME): " home;
+	if  [[ $home = '' ]]; then
+	    doIt $HOME;
+	else
+	    doIt $home;
+	fi;
     fi;
 fi;
 unset doIt;
