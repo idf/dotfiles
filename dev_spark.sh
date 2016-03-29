7b46e4708466b777babb0ba1a824d721f8e7c2c1
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2016-03-29 22:04:37 +0000

    chmod .local/share

--- dev_spark.sh
+++ dev_spark.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # reference: https://mas-dse.github.io/DSE230/installation/linux/
+sudo -v
 
 function start_jupyter() {
   # edit IP address: ~/.jupyter/jupyter_notebook_config.py: c.NotebookApp.ip = '0.0.0.0'
@@ -14,6 +15,7 @@ function start_jupyter() {
   export PYSPARK_DRIVER_PYTHON_OPTS="notebook"
   # Uncomment next line if the default python on your system is python3
   # export PYSPARK_PYTHON=python3
+  sudo chmod 777 $HOME/.local/
   cd "$1"
   $SPARK_PATH/bin/pyspark --master local[2]
 }
