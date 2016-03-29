f1213ee49024aa2b50576d22844b26fe94b6a15f
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2016-03-29 17:26:24 +0000

    spark dev env

---
+++ dev_spark.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# reference: https://mas-dse.github.io/DSE230/installation/linux/
+
+function start_jupyter() {
+  # edit IP address: ~/.jupyter/jupyter_notebook_config.py: c.NotebookApp.ip = '0.0.0.0'
+  # param: path
+  if [[ $# -eq 0 ]] ; then
+    echo 'Error: Please specify the work directory.'
+    return
+  fi
+
+  export SPARK_PATH="/usr"
+  export PYSPARK_DRIVER_PYTHON="jupyter"
+  export PYSPARK_DRIVER_PYTHON_OPTS="notebook"
+  # Uncomment next line if the default python on your system is python3
+  # export PYSPARK_PYTHON=python3
+  cd "$1"
+  $SPARK_PATH/bin/pyspark --master local[2]
+}
