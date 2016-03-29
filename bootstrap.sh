413a7cd29aa7c8009eb261f772bc2f2180f1fe3d
Daniel D. Zhang <idf@users.noreply.github.com>
2016-03-28 23:58:28 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-flag="-nfs";
-cur=$(pwd);
-cd $cur;
+flag="-nfs"
+cur=$(pwd)
+cd $cur
 
 function doIt() {
-    home=$1;
-    echo "Setting files in $home";
+    home=$1
+    echo "Setting files in $home"
     if [ "$(uname)" == "Darwin" ]; then
-	ln $flag "$cur/init" $home;
+	ln $flag "$cur/init" $home
     fi
 
     # usr bin files
-    ln $flag "$cur/bin" $home;
+    ln $flag "$cur/bin" $home
 
     # dotfiles
     for dotfile in .?*; do
@@ -26,29 +26,30 @@ function doIt() {
 	    .DS_Store)
 		continue;;
 	    *)
-		ln $flag "$cur/${dotfile}" $home;
+		ln $flag "$cur/${dotfile}" $home
 		;;
 	esac
     done
 
-    ln $flag "$cur/tmux-config/.tmux.conf" $home;
-    ln $flag "$cur/rc.py" "$home/.percol.d/";
-    source ~/.bashrc;
-    tmux source-file ~/.tmux.conf;  # TODO handle tmux not started
+    ln $flag "$cur/tmux-config/.tmux.conf" $home
+    mkdir -p "$home/.percol.d/"
+    ln $flag "$cur/rc.py" "$home/.percol.d/"
+    source ~/.bashrc
+    tmux source-file ~/.tmux.conf  # TODO handle tmux not started
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
     doIt $HOME;
 else
-    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
     echo "";
     if  [[ $REPLY=='Y' || $REPLY=='y' ]]; then
 	read -p "Home directory (default: $HOME): " home;
 	if  [[ $home = '' ]]; then
-	    doIt $HOME;
+	    doIt $HOME
 	else
-	    doIt $home;
+	    doIt $home
 	fi;
     fi;
 fi;
-unset doIt;
+unset doIt
