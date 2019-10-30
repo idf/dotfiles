a6298ef330afe8a614858d511425e300bb76aa47
Daniel D. ZHANG <zhangdanyangg@gmail.com>
2019-10-30 16:21:42 -0700

    move atom to bootstrap

--- atom/bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cur=$(pwd)
-flag="-nfs"
-ln $flag "$cur/keymap.cson" "$HOME/.atom/keymap.cson"
-ln $flag "$cur/styles.less" "$HOME/.atom/styles.less"
-
-bash $cur/pkgs.sh
-echo "Change font family to Fira Code"
